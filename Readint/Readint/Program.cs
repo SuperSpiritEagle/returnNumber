0d6e1c4faef55ddc454e5370f423c4ec262329e6
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-03-01 19:25:21 +0500

    init

---
+++ Readint/Readint/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Readint
+{
+    class Program
+    {
+        static int returnNumber()
+        {
+            bool isWork = true;
+            string userInput;
+            int number = 0;
+
+            while (isWork)
+            {
+                Console.WriteLine("Введите число");
+                userInput = Console.ReadLine();
+
+                Console.WriteLine();
+
+                bool isReadintResult = int.TryParse(userInput, out number);
+
+                if (isReadintResult == true)
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(returnNumber());
+        }
+    }
+}
