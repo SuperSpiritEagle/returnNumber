7934324ec8bc6c3c13eb74cbe2e6439419f24838
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-03-04 08:52:36 +0500

    !tryParse

--- Readint/Readint/Program.cs
+++ Readint/Readint/Program.cs
@@ -4,33 +4,24 @@ namespace Readint
 {
     class Program
     {
-        static int GetNumber()
+        static void Main(string[] args)
         {
-            bool isWork = true;
-            string userInput;
+
+            Console.WriteLine(GetNumber());
+
+        }
+        private static int GetNumber()
+        {
+            string userInput = "";
             int number = 0;
 
-            while (isWork)
+            while (!int.TryParse(userInput, out number))
             {
                 Console.WriteLine("Введите число");
-
                 userInput = Console.ReadLine();
-
-                Console.WriteLine();
-
-                bool isNumberResult = int.TryParse(userInput, out number);
-
-                if (isNumberResult == true)
-                {
-                    isWork = false;
-                }
             }
 
             return number;
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine(GetNumber());
         }
     }
 }
